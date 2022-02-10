c0807e3fa1e1d0675e867292fe4053d54eaef1e5
Yoshika <phpbbtw@gmail.com>
2022-02-10 18:04:11 +0800

    change conetnt

--- server.js
+++ server.js
@@ -2,15 +2,19 @@
 var net = require('net');
 
 var server = net.createServer(function(conn){
-  console.log('server-> tcp server created');
+  console.log('Server > TCP Server created');
 
   conn.on('data', function(data){
-    console.log('server-> ' + data + ' from ' + conn.remoteAddress + ':' + conn.remotePort);
-    conn.write('server -> Repeating: ' + data);
+    // console.log('server-> ' + data + ' from ' + conn.remoteAddress + ':' + conn.remotePort);
+    console.log('Server > ' + data + 'From'+' IP ' + conn.remoteAddress + ' Port: ' + conn.remotePort);
+    // conn.write('server -> Repeating: ' + data);
+    conn.write('Client > ' + data);
   });
+
   conn.on('close', function(){
-    console.log('server-> client closed connection');
+    // console.log('server-> client closed connection');
+    console.log('Client already closed connection T^T');
   });
 }).listen(3000);
 
-console.log('listening on port 3000');
+console.log('Listening on port 3000');
